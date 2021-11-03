ca98a85dae9fcb943b6cf7c48e9155f075425986
Rayhan Alcena <llcoolray24@gmail.com>
2021-11-03 03:07:57 -0400

    implemented expander

--- app.py
+++ app.py
@@ -8,28 +8,22 @@ def read_markdown_file(markdown_file):
 
 st.title("Fit Tracker")
 
-nf_md_check = st.checkbox("Nutritional Facts")
-nf_md = read_markdown_file("guide/1-Nutritional-Facts.md")
-
-mp_md_check = st.checkbox("Meal Plan")
-mp_md = read_markdown_file("guide/2-Meal-Plan.md")
-
-e_md_check = st.checkbox("Exercises")
-e_md = read_markdown_file("guide/3-Exercises.md")
-
-ee_md_check = st.checkbox("Exercise Equipment")
-ee_md = read_markdown_file("guide/4-Exercise-Equipment.md")
-
-ws_md_check = st.checkbox("Workout Schedule")
-ws_md = read_markdown_file("guide/5-Workout-Schedule.md")
-
-if nf_md_check:
+with st.expander("Nutritional Facts"):
+    nf_md = read_markdown_file("guide/1-Nutritional-Facts.md")
     st.markdown(nf_md, unsafe_allow_html=True)
-elif mp_md_check:
+
+with st.expander("Meal Plan"):
+    mp_md = read_markdown_file("guide/2-Meal-Plan.md")
     st.markdown(mp_md, unsafe_allow_html=True)
-elif e_md_check:
+
+with st.expander("Exercises"):
+    e_md = read_markdown_file("guide/3-Exercises.md")
     st.markdown(e_md, unsafe_allow_html=True)
-elif ee_md_check:
+
+with st.expander("Exercise Equipment"):
+    ee_md = read_markdown_file("guide/4-Exercise-Equipment.md")
     st.markdown(ee_md, unsafe_allow_html=True)
-elif ws_md_check:
+
+with st.expander("Workout Schedule"):
+    ws_md = read_markdown_file("guide/5-Workout-Schedule.md")
     st.markdown(ws_md, unsafe_allow_html=True)
