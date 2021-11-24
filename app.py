4e1942ec364ac9f8a5fb67c631e14b84aef3463f
iamllcoolray <llcoolray24@gmail.com>
2021-11-24 17:30:40 -0600

    restructured web app

--- app.py
+++ app.py
@@ -29,95 +29,97 @@ st.title("Fit Tracker")
 
 data_container = st.container()
 with data_container:
-    col1, col2 = st.columns(2)
 
-    with col1:
-        st.header("Data Input")
+    st.header("Tracker Entry")
 
-        st.subheader("How old are you?")
-        age = st.number_input("Age", 13, 130, 18)
+    st.subheader("How old are you?")
+    age = st.number_input("Age", 13, 130, 18)
 
-        st.subheader("What is your sex?")
-        sex = st.radio("Sex", ("Male", "Female"))
+    st.subheader("What is your sex?")
+    sex = st.radio("Sex", ("Male", "Female"))
 
-        st.subheader("How tall are you?")
-        feet_height = st.number_input("Feet", 4, 8, 6)
-        inches_height = st.number_input("Inches", 0, 11)
-        total_height = (feet_height * 12) + inches_height
+    st.subheader("How tall are you?")
+    feet_height = st.number_input("Feet", 4, 8, 6)
+    inches_height = st.number_input("Inches", 0, 11)
+    total_height = (feet_height * 12) + inches_height
 
-        st.subheader("How much do you weight?")
-        total_weight = st.number_input("Weight", 100, 400, 150)
+    st.subheader("How much do you weight?")
+    total_weight = st.number_input("Weight", 100, 400, 150)
 
-        st.subheader("How active are you each day?")
-        activity_level = st.selectbox(
-            "Activity Level", ("Sedentary", "Lightly Active", "Moderately Active", "Very Active", "Extremely Active"))
+    st.subheader("How active are you each day?")
+    activity_level = st.selectbox(
+        "Activity Level", ("Sedentary", "Lightly Active", "Moderately Active", "Very Active", "Extremely Active"))
 
-        st.subheader("What is your fitness goal?")
-        fitness_goal = st.radio(
-            "Fitness Goal", ("Maintain Current Weight", "Lose Weight/Body Fat", "Build Muscle"))
+    st.subheader("What is your fitness goal?")
+    fitness_goal = st.radio(
+        "Fitness Goal", ("Maintain Current Weight", "Lose Weight/Body Fat", "Build Muscle"))
 
-        lose_weight = ""
-        gain_weight = ""
-        approx_duration = 0
+    lose_weight = ""
+    gain_weight = ""
+    approx_duration = 0
 
-        if fitness_goal == "Lose Weight/Body Fat":
-            st.subheader("How much weight do you want to lose in a week?")
-            lose_weight = st.radio("Weight", ("1lbs", "2lbs"))
+    if fitness_goal == "Lose Weight/Body Fat":
+        st.subheader("How much weight do you want to lose in a week?")
+        lose_weight = st.radio("Weight", ("1lbs", "2lbs"))
 
-            st.subheader("What is your ideal weight?")
-            ideal_weight = st.number_input("Ideal Weight", 100, total_weight)
-        elif fitness_goal == "Maintain Current Weight":
-            pass
-        elif fitness_goal == "Build Muscle":
-            st.subheader("How much weight do you want to gain in a week?")
-            gain_weight = st.radio("Weight", ("1lbs", "2lbs"))
+        st.subheader("What is your ideal weight?")
+        ideal_weight = st.number_input("Ideal Weight", 100, total_weight)
+    elif fitness_goal == "Maintain Current Weight":
+        pass
+    elif fitness_goal == "Build Muscle":
+        st.subheader("How much weight do you want to gain in a week?")
+        gain_weight = st.radio("Weight", ("1lbs", "2lbs"))
 
-            st.subheader("What is your ideal weight?")
-            ideal_weight = st.number_input("Ideal Weight", total_weight, 400)
+        st.subheader("What is your ideal weight?")
+        ideal_weight = st.number_input("Ideal Weight", total_weight, 400)
 
-    with col2:
-        st.header("Your Results")
-        bmi = (total_weight / (total_height**2)) * 703
+results_container = st.container()
+with results_container:
 
-        if sex == "Male":
-            body_fat = ((1.2 * bmi) + (0.23 * age)) - 16.2
-            bmr = 66.47 + (6.24 * total_weight) + \
-                (12.7 * total_height) - (6.755 * age)
-        elif sex == "Female":
-            body_fat = ((1.2 * bmi) + (0.23 * age)) - 5.4
-            bmr = 655.1 + (4.35 * total_weight) + \
-                (4.7 * total_height) - (4.7 * age)
+    st.header("Personalized Results")
+    bmi = (total_weight / (total_height**2)) * 703
 
-        if activity_level == "Sedentary":
-            caloric_intake = bmr * 1.2
-        elif activity_level == "Lightly Active":
-            caloric_intake = bmr * 1.375
-        elif activity_level == "Moderately Active":
-            caloric_intake = bmr * 1.55
-        elif activity_level == "Very Active":
-            caloric_intake = bmr * 1.725
-        elif activity_level == "Extremely Active":
-            caloric_intake = bmr * 1.9
+    if sex == "Male":
+        body_fat = ((1.2 * bmi) + (0.23 * age)) - 16.2
+        bmr = 66.47 + (6.24 * total_weight) + \
+            (12.7 * total_height) - (6.755 * age)
+    elif sex == "Female":
+        body_fat = ((1.2 * bmi) + (0.23 * age)) - 5.4
+        bmr = 655.1 + (4.35 * total_weight) + \
+            (4.7 * total_height) - (4.7 * age)
 
-        if lose_weight == "1lbs":
-            caloric_intake -= 500
-            approx_duration = losing_weight(1)
-        elif lose_weight == "2lbs":
-            caloric_intake -= 1000
-            approx_duration = losing_weight(2)
-        elif gain_weight == "1lbs":
-            caloric_intake += 500
-            approx_duration = gaining_weight(1)
-        elif gain_weight == "2lbs":
-            caloric_intake += 1000
-            approx_duration = gaining_weight(2)
+    if activity_level == "Sedentary":
+        caloric_intake = bmr * 1.2
+    elif activity_level == "Lightly Active":
+        caloric_intake = bmr * 1.375
+    elif activity_level == "Moderately Active":
+        caloric_intake = bmr * 1.55
+    elif activity_level == "Very Active":
+        caloric_intake = bmr * 1.725
+    elif activity_level == "Extremely Active":
+        caloric_intake = bmr * 1.9
 
-        st.metric("Body Fat Percentage", str(round(body_fat)) + "%")
-        st.metric("Body Mass Index", round(bmi))
-        st.metric("Basal Metabolic Rate", round(bmr))
-        st.metric("Caloric Intake", round(caloric_intake))
-        if round(approx_duration) > 0:
-            st.metric("Approximate Duration", round(approx_duration))
+    if lose_weight == "1lbs":
+        caloric_intake -= 500
+        approx_duration = losing_weight(1)
+    elif lose_weight == "2lbs":
+        caloric_intake -= 1000
+        approx_duration = losing_weight(2)
+    elif gain_weight == "1lbs":
+        caloric_intake += 500
+        approx_duration = gaining_weight(1)
+    elif gain_weight == "2lbs":
+        caloric_intake += 1000
+        approx_duration = gaining_weight(2)
+
+    col1, col2, col3, col4 = st.columns(4)
+
+    col1.metric("Body Fat Percentage", str(round(body_fat)) + "%")
+    col2.metric("Body Mass Index", round(bmi))
+    col3.metric("Basal Metabolic Rate", round(bmr))
+    col4.metric("Caloric Intake", round(caloric_intake))
+    if round(approx_duration) > 0:
+        st.metric("Approximate Duration", round(approx_duration))
 
 info_container = st.container()
 with info_container:
